3c6f9f701122a7f82facbd5fca96720efcfcf9a4
atillaertas1 <atillaertas@protonmail.com>
2024-07-12 21:27:53 +0300

    question added

--- src/problem3.c
+++ src/problem3.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+
 int main()
 {
     long long number = 600851475143;
+    printf("%lld",number);
     int largest_prime = 0;
 
     if (number % 2 == 0)
@@ -11,23 +15,23 @@ int main()
         number = number / 2;
     }
 
-    printf("number -> %d\n", number);
+    printf("number -> %lld\n", number);
     for (int i = 3; i * i <= number; i += 2)
     {
         printf("i -> %d\n", i);
-        while(number % i == 0)
+        while (number % i == 0)
         {
             printf("---------------------------\n");
             largest_prime = i;
             printf("largest_prime -> %d\n", largest_prime);
             number = number / i;
-            printf("number -> %d\n", number);
+            printf("number -> %lld\n", number);
             printf("---------------------------\n");
         }
     }
 
-
-    if (number > largest_prime){
+    if (number > largest_prime)
+    {
         largest_prime = number;
     }
     printf("prime -- %d\n", largest_prime);
